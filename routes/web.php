90e6caace0f6d430b22b4f56207d9695073a6ab8
Clement-Leger <clementleger4@gmail.com>
2023-03-09 16:38:03 +0100

    premiers pas

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', function() {
+//     return 'Je suis la page d\'accueil !';
+//   })->name('home');
+
+Route::get('{i?}', function($i=0) { return 'Je suis la page ' . $i . '!'; })->where('i', '[1-3]');
+
+Route::get('contact', function() {
+    return "Moi c'est le contact.";
+});
