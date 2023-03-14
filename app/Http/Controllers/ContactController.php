7d8350e7ac2b50d7ced12ef3b707a72bacbee5cb
Clement-Leger <clementleger4@gmail.com>
2023-03-14 14:50:13 +0100

    second from

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,8 +13,13 @@ class ContactController extends Controller
         return view('contact');
     }
 
-    public function store(ContactRequest $request) : View
+    public function store(Request $request) : View
     {
+        $this->validate($request, [
+            'nom' => 'bail|required|between:5,20|alpha',
+            'email' => 'bail|required|email',
+            'message' => 'bail|required|max:250'
+        ]);
         return view('confirm');
     }
 }
